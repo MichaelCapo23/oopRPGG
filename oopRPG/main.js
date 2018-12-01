22fa56198a1597143fa89522a245c38b2d912610
MichaelCapo23 <123pie23@gmail.com>
2018-12-01 12:25:46 -0800

    some dom creation, character class set up

---
+++ oopRPG/main.js
@@ -0,0 +1,16 @@
+$(document).ready(startApp);
+
+function startApp() {
+    domCreate();
+}
+function domCreate() {
+    var player1Indicator = $("<div>", {
+        "class" : "player1Indicator",
+        "text" : "player 1"
+    });
+
+    var player2Indicator = $("<div>", {
+        "class" : "player2Indicator",
+        "text": "player 2"
+    })
+}
